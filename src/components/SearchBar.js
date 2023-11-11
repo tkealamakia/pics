d4d2a34dcd776894a5b257b677f5830e01964148
Tom Kealamakia <tkealamakia@sharktank.tsunazumi.com>
2023-11-10 20:00:23 -0700

    message

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,20 @@
+import { useState } from 'react';
+
 function SearchBar( { onSubmit }) {
+  const[term, setTerm] = useState('');
   const handleFormSubmit = (event) => {
     event.preventDefault();
     onSubmit('cars');
   }
+
+  const handleChange = (event) => {
+    setTerm(event.target.value);
+  };
+
   return (
     <div>
       <form onSubmit={handleFormSubmit}>
-        <input />
+        <input value={term} onChange={handleChange} />
       </form>
       <button onClick={handleFormSubmit}>Click me</button>
     </div>
